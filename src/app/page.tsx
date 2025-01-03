10a8083643398807bdfe2967516e2a473f698659
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-03 11:21:26 -0700

    allow save of card, display in ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+'use client'
+import { useState } from "react";
 import { IdeaForm } from "./components/IdeaForm";
 import { IdeaCard } from "./components/IdeaCard";
+import type { Card } from "@/utils";
 
 export default function Home() {
+  const [cards, setCards] = useState<Card[]>([]);
+
+  const saveCard = (newCard: Card) => {
+    setCards([...cards, newCard])
+  };
+  
   return (
     <div className="">
-      <IdeaForm />
-      <IdeaCard />
+      <IdeaForm saveCard={saveCard}/>
+
+      {cards.length ? cards.map((card: Card) => <IdeaCard key={card.id} card={card} />) : <></>}
+
     </div>
   );
 }
