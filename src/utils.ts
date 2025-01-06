63a3032352323c20c5c4f01f339ba8ba99605bf4
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-06 13:39:06 -0700

    Create a find card function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
 export type Card = {
   text: string
   id: number
-}
+  isFavorite: boolean
+};
+
+export const findCard = (id: number, cards: Card[]) => {
+
+  const cardToFind = cards.find(card => card.id === id);
+  return cardToFind || null;
+
+};
