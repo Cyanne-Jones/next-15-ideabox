2550bee2d18de936e602aea25d9d6361c9a45a59
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-07 10:41:05 -0700

    retrieve and parse cards from local storage

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,9 +1,30 @@
+'use client'
+import Link from "next/link";
+import { IdeaCard } from "../components/IdeaCard";
+import { Card } from "@/utils";
+
 const Favorites = () => {
-  const cats = 'meow'
+
+  let favorites = ''
+
+  if (typeof window !== undefined) {
+    try {
+    favorites = localStorage.getItem('favorites') || '';
+    const cards = localStorage.getItem('cards');
+    } catch(e) {
+      console.log(e)
+    }
+  }
+  
 
   return (
     <div>
       this is the favorites page! 
+      <Link className='cursor-pointer text-pink-500 hover:text-purple-200' href='/'>Home!</Link>
+
+      <div className='flex max-w-lg flex-wrap items-center justify-center'>
+        {favorites.length ? JSON.parse(favorites || '').map((card: Card) => <IdeaCard key={card.id} card={card}/>) : <></>}
+      </div>
     </div>
   )
 }
