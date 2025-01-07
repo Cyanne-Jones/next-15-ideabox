b406f8f70e786774b4d64ecbeb69f8f33092b13e
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-07 10:40:40 -0700

    make setter functions optional in card component

--- src/app/components/IdeaCard.tsx
+++ src/app/components/IdeaCard.tsx
@@ -8,8 +8,8 @@ export const IdeaCard = ({
   deleteCard} 
   : {
     card: Card
-    favoriteCard: (id: number) => void
-    deleteCard: (id: number) => void
+    favoriteCard?: (id: number) => void
+    deleteCard?: (id: number) => void
   }) => {
 
   const buttonStyle = 'min-h-8 min-w-8 rounded-lg bg-pink-200 text-purple-700 hover:bg-pink-500 hover:text-purple-900 flex items-center justify-center'
@@ -17,18 +17,18 @@ export const IdeaCard = ({
   return (
     <div className='min-h-32 w-32 bg-purple-400 rounded-lg m-2 p-2'>
       <div className='flex justify-between'>
-        <button 
+        {favoriteCard && <button 
           onClick={() => favoriteCard(card.id)}
           className={buttonStyle}
           >
           {card.isFavorite ? '★' : '☆'}
-        </button>
-        <button 
+        </button>}
+        {deleteCard && <button 
           onClick={() => deleteCard(card.id)}
           className={buttonStyle}
           >
           X
-        </button>
+        </button>}
       </div>
       {card.text}
     </div>
