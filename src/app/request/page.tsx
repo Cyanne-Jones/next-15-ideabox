89d44a6094d81f2ebd0a1f15a316ccd8ec841e65
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-09 11:04:24 -0700

    Update request page to be server side and async, request cat image, show cat image

--- src/app/request/page.tsx
+++ src/app/request/page.tsx
@@ -1,17 +1,26 @@
-'use client'
+import Image from "next/image";
 
-const Request = () => {
+export default async function RequestPage() {
 
-  // request nice data here
+  const response = await fetch('https://api.thecatapi.com/v1/images/search', {
+    cache: 'no-store', // Disable caching for fresh data
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch cat data');
+  }
+
+  const data = await response.json();
 
   return (
     <div>
-      <h1>
-        This is the request page!
-      </h1>
+      <h1>This is the request page!</h1>
+      <Image
+        src={data[0].url}
+        alt='cat'
+        height={data[0].height}
+        width={data[0].width}
+      />
     </div>
-  )
-
+  );
 };
-
-export default Request;
