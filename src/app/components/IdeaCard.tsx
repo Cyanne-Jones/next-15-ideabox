687bb67f855515ea1b130ab6ce22bc88ae708e69
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-09 11:29:51 -0700

    Give buttons box shadow and give cards borders

--- src/app/components/IdeaCard.tsx
+++ src/app/components/IdeaCard.tsx
@@ -12,10 +12,10 @@ export const IdeaCard = ({
     deleteCard?: (id: number) => void
   }) => {
 
-  const buttonStyle = 'min-h-8 min-w-8 rounded-lg bg-pink-200 text-purple-700 hover:bg-pink-500 hover:text-purple-900 flex items-center justify-center'
+  const buttonStyle = 'min-h-8 min-w-8 rounded-lg bg-pink-200 text-purple-700 hover:bg-pink-500 hover:text-purple-900 flex items-center justify-center hover:shadow-md hover:shadow-pink-700'
 
   return (
-    <div className='min-h-32 w-32 bg-purple-400 rounded-lg m-2 p-2'>
+    <div className='min-h-32 w-32 bg-purple-400 rounded-lg m-2 p-2 border-2 border-purple-700'>
       <div className='flex justify-between'>
         {favoriteCard && <button 
           onClick={() => favoriteCard(card.id)}
