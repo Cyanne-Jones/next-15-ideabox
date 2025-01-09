ac971d46301c0b47258c897a0109dd74da47c14a
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-09 11:03:46 -0700

    configure hostname prop in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn2.thecatapi.com',
+        port: '',
+        pathname: '/images/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
